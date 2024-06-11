b298590511609a4d4c735df3f07347a8f1c70689
moonhr <moonhr153@gmail.com>
2024-06-11 15:27:21 +0900

    flat() 은 배열의 내부를 까내서 배열을만듦

---
+++ array/flat().js
@@ -0,0 +1,18 @@
+//* 모든 하위 배열 요소가 지정된 깊이까지 재귀적으로 연결된 새 배열을 생성합니다.
+//* 느리다!!
+
+const arr1 = [0, 1, 2, [3, 4]];
+
+console.log(arr1.flat());
+// expected output: Array [0, 1, 2, 3, 4]
+
+const arr2 = [0, 1, [2, [3, [4, 5]]]];
+
+console.log(arr2.flat());
+// expected output: Array [0, 1, 2, Array [3, Array [4, 5]]]
+
+console.log(arr2.flat(2));
+// expected output: Array [0, 1, 2, 3, Array [4, 5]]
+
+console.log(arr2.flat(Infinity));
+// expected output: Array [0, 1, 2, 3, 4, 5]
