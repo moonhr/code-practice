dccbca39a5eb8a538e006d6decc0da6e082b5a4f
moonhr <moonhr153@gmail.com>
2024-06-11 14:49:54 +0900

    find() 메서드의 4가지 변형

--- array/find().js
+++ array/find().js
@@ -11,3 +11,39 @@ const found = array1.find((element) => element > 10);
 
 console.log(found);
 // Expected output: 12
+
+
+
+findIndex() 
+//* 메서드는 주어진 판별 함수를 만족하는 배열의 첫 번째 요소에 대한 인덱스를 반환합니다. 만족하는 요소가 없으면 -1을 반환합니다.
+const array2 = [5, 12, 8, 130, 44];
+
+const isLargeNumber = (element) => element > 13;
+
+console.log(array2.findIndex(isLargeNumber));
+// Expected output: 3
+
+
+
+
+findLast() 
+//* 메서드는 배열을 역순으로 순회하며 제공된 테스트 함수를 만족하는 첫 번째 요소의 값을 반환합니다. 테스트 함수를 만족하는 요소가 없으면 undefined가 반환됩니다.
+const array3 = [5, 12, 50, 130, 44];
+
+const foun = array3.findLast((element) => element > 45);
+
+console.log(foun);
+// Expected output: 130
+
+
+
+
+findLastIndex() 
+//* 메서드는 배열을 역순으로 순회하며 주어진 판별 함수를 만족하는 만족하는 배열의 첫번째 요소의 인덱스를 반환합니다. 만족하는 요소가 없으면 -1을 반환합니다.
+const array4 = [5, 12, 50, 130, 44];
+
+const isLargeNumbe = (element) => element > 45;
+
+console.log(array4.findLastIndex(isLargeNumbe));
+// Expected output: 3
+// Index of element with value: 130
