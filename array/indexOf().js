bbe4d0281e44fe811505e13ae67118eac9fb8a1d
moonhr <moonhr153@gmail.com>
2024-06-11 16:43:51 +0900

    배열의 특정 요소를 찾기

---
+++ array/indexOf().js
@@ -0,0 +1,13 @@
+//* 배열에서 주어진 요소를 찾을 수 있는 첫 번째 인덱스를 반환하고, 찾을 수 없는 경우 -1을 반환
+
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+console.log(beasts.indexOf('bison'));
+// Expected output: 1
+
+// Start from index 2
+console.log(beasts.indexOf('bison', 2));
+// Expected output: 4
+
+console.log(beasts.indexOf('giraffe'));
+// Expected output: -1
