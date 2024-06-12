5dbc880a7c8167b99b62df88438ca669f4aff9f8
moonhr <moonhr153@gmail.com>
2024-06-12 17:39:00 +0900

    배열의 첫번쨍 요소 제거후 반환

---
+++ array/shift().js
@@ -0,0 +1,11 @@
+//* 배열에서 첫 번째 요소를 제거하고, 제거된 요소를 반환합니다. 이 메서드는 배열의 길이를 변하게 합니다.
+
+const array1 = [1, 2, 3];
+
+const firstElement = array1.shift();
+
+console.log(array1);
+// Expected output: Array [2, 3]
+
+console.log(firstElement);
+// Expected output: 1
