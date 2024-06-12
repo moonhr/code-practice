a3050058750947b8b4a560689977e6aedaf9d5c7
moonhr <moonhr153@gmail.com>
2024-06-12 16:52:42 +0900

    배열의 쉼표제거할때 주로씀

---
+++ array/join().js
@@ -0,0 +1,12 @@
+//*  배열의 모든 요소를 쉼표나 지정된 구분 문자열로 구분하여 연결한 새 문자열을 만들어 반환
+
+const elements = ['Fire', 'Air', 'Water'];
+
+console.log(elements.join());
+// Expected output: "Fire,Air,Water"
+
+console.log(elements.join(''));
+// Expected output: "FireAirWater"
+
+console.log(elements.join('-'));
+// Expected output: "Fire-Air-Water"
