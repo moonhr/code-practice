655c95a7e00bf4220367aeffefe5d52ede7c0807
moonhr <moonhr153@gmail.com>
2024-06-12 17:23:48 +0900

    배열에 요소 추가

---
+++ array/push().js
@@ -0,0 +1,13 @@
+//* 배열의 끝에 하나 이상의 요소를 추가하고, 배열의 새로운 길이를 반환
+
+const animals = ['pigs', 'goats', 'sheep'];
+
+const count = animals.push('cows');
+console.log(count);
+// Expected output: 4
+console.log(animals);
+// Expected output: Array ["pigs", "goats", "sheep", "cows"]
+
+animals.push('chickens', 'cats', 'dogs');
+console.log(animals);
+// Expected output: Array ["pigs", "goats", "sheep", "cows", "chickens", "cats", "dogs"]
