17e836670d94b790b1c5a864c4f1b75fdabaeb62
moonhr <moonhr153@gmail.com>
2024-06-12 17:23:06 +0900

    마지막 요소 제거 및 반환

---
+++ array/pop().js
@@ -0,0 +1,16 @@
+//* 배열에서 마지막 요소를 제거하고 그 요소를 반환
+
+const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+
+console.log(plants.pop());
+// Expected output: "tomato"
+
+console.log(plants);
+// Expected output: Array ["broccoli", "cauliflower", "cabbage", "kale"]
+
+plants.pop();
+
+console.log(plants);
+// Expected output: Array ["broccoli", "cauliflower", "cabbage"]
+
+
