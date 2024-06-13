2f05f6d5ff5de743084416eb2607cf296429ddd1
moonhr <moonhr153@gmail.com>
2024-06-13 12:09:45 +0900

    배열의 요소 정렬하기

---
+++ array/sort().js
@@ -0,0 +1,51 @@
+// * 배열의 요소를 적절한 위치에 정렬한 후 그 배열을 반환 기본 정렬 순서는 문자열의 유니코드 코드 포인트
+// * 순서 정렬할때 사용
+
+
+const months = ['March', 'Jan', 'Feb', 'Dec'];
+months.sort();
+console.log(months);
+// Expected output: Array ["Dec", "Feb", "Jan", "March"]
+
+const array1 = [1, 30, 4, 21, 100000];
+array1.sort();
+console.log(array1);
+// Expected output: Array [1, 100000, 21, 30, 4]
+
+
+//* 하나의 값을 기준으로 정렬
+var items = [
+  { name: "Edward", value: 21 },
+  { name: "Sharpe", value: 37 },
+  { name: "And", value: 45 },
+  { name: "The", value: -12 },
+  { name: "Magnetic", value: 13 },
+  { name: "Zeros", value: 37 },
+];
+
+// value 기준으로 정렬
+items.sort(function (a, b) {
+  if (a.value > b.value) {
+    return 1;
+  }
+  if (a.value < b.value) {
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+});
+
+// name 기준으로 정렬
+items.sort(function (a, b) {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // 이름이 같을 경우
+  return 0;
+});
