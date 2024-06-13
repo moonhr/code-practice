6a76d672dd7b0c3d5056756b51194e1d1b986be0
moonhr <moonhr153@gmail.com>
2024-06-13 12:48:50 +0900

    배열의 맨앞에 추가

---
+++ array/unshift().js
@@ -0,0 +1,9 @@
+//* 새로운 요소를 배열의 맨 앞쪽에 추가하고, 새로운 길이를 반환
+
+const array1 = [1, 2, 3];
+
+console.log(array1.unshift(4, 5));
+// Expected output: 5
+
+console.log(array1);
+// Expected output: Array [4, 5, 1, 2, 3]
