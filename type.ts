baa72c20ef5d1dcae054c186646b4ce226dcf01d
moonhr <moonhr153@gmail.com>
2024-07-02 10:12:45 +0900

    typq코드 작성

--- type.ts
+++ type.ts
@@ -1,3 +1,29 @@
 function isIntegerCheck(value: number): boolean{
-  
+  let checkValue : boolean = Number.isInteger(value);
+  return checkValue;
+}
+
+//아래 함수 선언부에서 '함수'가 무엇을 입력하고 출력하는지 한눈에 알아볼 수 있다.
+function add (a:number, b:number):number{
+  if(isIntegerCheck(a)===false || isIntegerCheck(b)===false){
+    throw new Error("매개변수 둘 중 하나라도 정수가 아니라면 에러를 발생시키기");
+  }
+  let result:number = a+b;
+  return result
+}
+
+//실행부분
+try {
+  const result: number = add(2,3);
+  console.log(result);
+
+  //의도하지 않은 인자타입을 전달하면 컴파일 타임에 오류가 발생
+  //const resultWithString :number = (2,'3');
+  //오류: Argument of type 'string' is not assignable to parameter of type 'number'.
+
+  //소수의 값을 전달하면 의도하지 않았음으로 에러 발생 
+  const resultWithString:number = add (2.5, 3);
+  console.log(resultWithString);
+} catch (error) {
+  console.error(error.message);
 }
